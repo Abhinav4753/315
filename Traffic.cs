c59524d021b4eb25da0dae1b8fe2142c46e1e283
Abhinav4753 <133110792+Abhinav4753@users.noreply.github.com>
2024-03-04 23:55:23 +0530

    Update Traffic.cs

--- Traffic.cs
+++ Traffic.cs
@@ -1,126 +1,118 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 
-// Class to represent the traffic signal data
-class TrafficSignalData
-{
-    public DateTime Timestamp { get; set; } // Timestamp of the data
-    public int TrafficLightId { get; set; } // ID of the traffic light
-    public int CarsPassed { get; set; } // Number of cars that passed the traffic light
+#define BUFFER_SIZE 1
+#define MAX_LIGHTS 10
+#define NUM_PRODUCERS 2
+#define NUM_CONSUMERS 2
+
+typedef struct {
+    time_t timestamp;
+    int lightId;
+    int carsPassed;
+} TrafficData;
+
+typedef struct {
+    TrafficData data[BUFFER_SIZE];
+    int in, out;
+    sem_t mutex, full, empty;
+} BoundedBuffer;
+
+BoundedBuffer buffer;
+pthread_mutex_t congestionDataLock = PTHREAD_MUTEX_INITIALIZER;
+int congestionData[MAX_LIGHTS];
+
+time_t currentTime; // Global variable to store current time
+
+void initBuffer(BoundedBuffer* buffer) {
+    buffer->in = 0;
+    buffer->out = 0;
+    sem_init(&buffer->mutex, 0, 1);
+    sem_init(&buffer->full, 0, 0);
+    sem_init(&buffer->empty, 0, BUFFER_SIZE);
 }
 
-class TrafficControl
-{
-    private const int BufferSize = 1; // Size of the buffer
-    private const int NumberOfProducers = 7; // Number of producer threads
-    private const int NumberOfConsumers = 4; // Number of consumer threads
+void produce(BoundedBuffer* buffer) {
+    struct timespec currentTimeSpec;
+    clock_gettime(CLOCK_REALTIME, &currentTimeSpec);
 
-    private static Queue<TrafficSignalData> buffer = new Queue<TrafficSignalData>(BufferSize); // Buffer to store the data
-    private static SortedList<int, int> congestionData = new SortedList<int, int>(); // Data to store congestion information
+    for (int i = 0; i < 12; i++) {
+        TrafficData newData = {
+            .timestamp = currentTimeSpec.tv_sec + (i * 300), // 5 minutes interval
+            .lightId = rand() % MAX_LIGHTS + 1,
+            .carsPassed = rand() % 100 + 1,
+        };
 
-    private static object bufferLock = new object(); // Lock for the buffer
-    private static object congestionDataLock = new object(); // Lock for the congestion data
+        sem_wait(&buffer->empty);
+        sem_wait(&buffer->mutex);
 
-    private static ManualResetEvent producerEvent = new ManualResetEvent(false); // Event to signal when data is available for consumers
-    private static ManualResetEvent consumerEvent = new ManualResetEvent(false); // Event to signal when there is space available in the buffer for producers
+        buffer->data[buffer->in] = newData;
+        buffer->in = (buffer->in + 1) % BUFFER_SIZE;
 
-    static void Main()
-    {
-        // Create producer threads
-        Thread[] producerThreads = new Thread[NumberOfProducers];
-        for (int i = 0; i < NumberOfProducers; i++)
-        {
-            producerThreads[i] = new Thread(ProducerThread);
-            producerThreads[i].Start();
+        sem_post(&buffer->mutex);
+        sem_post(&buffer->full);
+
+        usleep(rand() % 500000 + 500000); // Simulate delay between measurements
+    }
+}
+
+
+void consume(BoundedBuffer* buffer) {
+    while (1) {
+        sem_wait(&buffer->full);
+        sem_wait(&buffer->mutex);
+
+        TrafficData data = buffer->data[buffer->out];
+        buffer->out = (buffer->out + 1) % BUFFER_SIZE;
+
+        sem_post(&buffer->mutex);
+        sem_post(&buffer->empty);
+
+        pthread_mutex_lock(&congestionDataLock);
+
+        if (congestionData[data.lightId] != 0) {
+            congestionData[data.lightId] += data.carsPassed;
+        } else {
+            congestionData[data.lightId] = data.carsPassed;
         }
 
-        // Create consumer threads
-        Thread[] consumerThreads = new Thread[NumberOfConsumers];
-        for (int i = 0; i < NumberOfConsumers; i++)
-        {
-            consumerThreads[i] = new Thread(ConsumerThread);
-            consumerThreads[i].Start();
-        }
+        pthread_mutex_unlock(&congestionDataLock);
 
-        // Wait for all producer threads to finish
-        for (int i = 0; i < NumberOfProducers; i++)
-        {
-            producerThreads[i].Join();
-        }
+        char timestampStr[20];
+        strftime(timestampStr, sizeof(timestampStr), "%Y-%m-%d %H:%M:%S", localtime(&data.timestamp));
+        printf("Data - Time: %s, LightId: %d, CarsPassed: %d\n", timestampStr, data.lightId, data.carsPassed);
 
-        // Notify consumers that no more data will be produced
-        producerEvent.Set();
+        usleep(100000); // Simulate processing time
+    }
+}
 
-        // Wait for all consumer threads to finish
-        for (int i = 0; i < NumberOfConsumers; i++)
-        {
-            consumerThreads[i].Join();
-        }
+int main() {
+    initBuffer(&buffer);
+
+    pthread_t producerThreads[NUM_PRODUCERS];
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        pthread_create(&producerThreads[i], NULL, (void*)&produce, &buffer);
     }
 
-    static void ProducerThread()
-    {
-        Random rand = new Random();
-        for (int i = 0; i < 12; i++) // Produce 12 data items
-        {
-            TrafficSignalData newData = new TrafficSignalData
-            {
-                Timestamp = DateTime.Now.AddMinutes(5 * i), // Generate a timestamp for the data
-                TrafficLightId = rand.Next(1, 11), // Generate a random traffic light ID between 1 and 10
-                CarsPassed = rand.Next(1, 101) // Generate a random number of cars passed between 1 and 100
-            };
-
-            lock (bufferLock) // Lock the buffer to prevent race conditions
-            {
-                while (buffer.Count == BufferSize) // Wait if the buffer is full
-                {
-                    Monitor.Wait(bufferLock);
-                }
-
-                buffer.Enqueue(newData); // Add the data to the buffer
-                producerEvent.Set(); // Signal that data is available for consumers
-                Monitor.Pulse(bufferLock); // Notify a waiting thread that the buffer has been modified
-            }
-
-            Thread.Sleep(rand.Next(500, 1000)); // Simulate some delay before producing the next data
-        }
+    pthread_t consumerThreads[NUM_CONSUMERS];
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        pthread_create(&consumerThreads[i], NULL, (void*)&consume, &buffer);
     }
 
-    static void ConsumerThread()
-    {
-        while (true) // Run indefinitely
-        {
-            producerEvent.WaitOne(); // Wait for data to be available from producers
-
-            lock (bufferLock) // Lock the buffer to prevent race conditions
-            {
-                while (buffer.Count == 0) // Wait if the buffer is empty
-                {
-                    Monitor.Wait(bufferLock);
-                }
-
-                TrafficSignalData data = buffer.Dequeue(); // Get the data from the buffer
-                consumerEvent.Set(); // Signal that there is space available in the buffer for producers
-                Monitor.Pulse(bufferLock); // Notify a waiting thread that the buffer has been modified
-
-                lock (congestionDataLock) // Lock the congestion data to prevent race conditions
-                {
-                    if (congestionData.ContainsKey(data.TrafficLightId)) // If the traffic light ID already exists in the congestion data
-                    {
-                        congestionData[data.TrafficLightId] += data.CarsPassed; // Update the number of cars passed for that traffic light ID
-                    }
-                    else
-                    {
-                        congestionData.Add(data.TrafficLightId, data.CarsPassed); // Add a new entry for the traffic light ID
-                    }
-                }
-
-                Console.WriteLine($"Data - Time: {data.Timestamp}, LightId: {data.TrafficLightId}, CarsPassed: {data.CarsPassed}"); // Print the data
-
-                Thread.Sleep(100); // Simulate some processing time
-            }
-        }
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        pthread_join(producerThreads[i], NULL);
     }
-    
+
+    pthread_mutex_lock(&congestionDataLock);
+    // Notify consumers that no more data will be produced
+    pthread_mutex_unlock(&congestionDataLock);
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        pthread_join(consumerThreads[i], NULL);
+    }
+
+    return 0;
 }
